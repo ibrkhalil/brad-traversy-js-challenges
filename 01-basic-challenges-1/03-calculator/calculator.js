868258914fe4d7916ac29dee03707ad423a96555
ibrkhalil <vampirekid017@gmail.com>
2024-06-09 11:57:42 +0300

    Calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,20 @@
-function calculator() {}
+function calculator(x, y, op) {
+  switch (op) {
+    case "+":
+      return x + y;
+      break;
+    case "-":
+      return x - y;
+      break;
+    case "*":
+      return x * y;
+      break;
+    case "/":
+      return x / y;
+      break;
+    default:
+      throw new Error("Invalid operator");
+  }
+}
 
 module.exports = calculator;
