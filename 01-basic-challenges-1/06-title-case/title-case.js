3463326fff9db3ca9dfbd023eeb5f5c76fb301b7
ibrkhalil <vampirekid017@gmail.com>
2024-06-09 12:10:30 +0300

    Title case

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(str) {
+    const wordArr = str.split(' ');
+    for(let i = 0; i < wordArr.length; ++i) {
+        wordArr[i] = wordArr[i].charAt(0).toUpperCase() + wordArr[i].slice(1);
+    }
+    return wordArr.join(' ');
+    
+}
 
 module.exports = titleCase;
