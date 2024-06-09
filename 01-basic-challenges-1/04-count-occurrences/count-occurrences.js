91d95bcb47a147184b14bb57226b908943fb93d3
ibrkhalil <vampirekid017@gmail.com>
2024-06-09 11:59:30 +0300

    Count occurences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,12 @@
-function countOccurrences() {}
+function countOccurrences(string, target) {
+    const length = string.length
+    let counter = 0;
+    for (let i = 0; i < length; ++i) {
+        if (string[i] == target) {
+            counter++;
+        }
+      }
+      return counter;
+}
 
 module.exports = countOccurrences;
