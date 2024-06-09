d64f021faf95ba770bf0088469f3c026d044e189
ibrkhalil <vampirekid017@gmail.com>
2024-06-09 12:36:33 +0300

    Count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,16 @@
-function countVowels() {}
+function countVowels(str) {
+    const vowels = ['a', 'e', 'o', 'i', 'u'];
+    let counter = 0;
+    let formattedString = str.toLowerCase()
+    for (let index = 0; index < formattedString.length; index++) {
+        if(formattedString[index] == vowels[0] ||
+            formattedString[index] == vowels[1] ||
+            formattedString[index] == vowels[2] ||
+            formattedString[index] == vowels[3] ||
+            formattedString[index] == vowels[4])
+            counter++;
+    }
+    return counter;
+}
 
 module.exports = countVowels;
