9fbc4e8ae18065cf9309461f5ae2c431fe37cde1
ibrkhalil <vampirekid017@gmail.com>
2024-06-10 20:30:46 +0300

    Are all chars unique?

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,10 @@
-function areAllCharactersUnique() {}
+function areAllCharactersUnique(str) {
+  const strToCharArr = [];
+  for (let index = 0; index < str.length; index++) {
+    strToCharArr.push(str[index]);
+  }
+  const comparisonSet = new Set(strToCharArr);
+  return comparisonSet.size === strToCharArr.length;
+}
 
 module.exports = areAllCharactersUnique;
