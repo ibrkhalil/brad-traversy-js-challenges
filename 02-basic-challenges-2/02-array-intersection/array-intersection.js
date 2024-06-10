9a44cc94650148ec5d295d436111fa85da8a070c
ibrkhalil <vampirekid017@gmail.com>
2024-06-10 19:27:23 +0300

    Intersection ar

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,14 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+    let intersection = []
+    for (let index = 0; index < arr1.length; index++) {
+        const element = arr1[index];
+        for (let j = 0; j < arr2.length; j++) {
+            if(element === arr2[j])
+                intersection.push(element);
+        }
+    }
+    const resSet = new Set(intersection);
+    return Array.from(resSet);
+}
 
 module.exports = arrayIntersection;
