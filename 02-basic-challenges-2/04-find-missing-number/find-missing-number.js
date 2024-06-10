c00eadae5f9a0dddc940a21171a808412b35932e
ibrkhalil <vampirekid017@gmail.com>
2024-06-10 19:55:46 +0300

    Find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,9 @@
-function findMissingNumber() {}
+function findMissingNumber(array) {
+  const max = array.reduce((a, b) => { return Math.max(a, b) });
+  for (let index = 1; index <= max; index++) {
+    if(!array.includes(index))
+        return index
+  } 
+}
 
 module.exports = findMissingNumber;
