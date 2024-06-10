48d2e69055a945ecc842f85ac84680644568e66a
ibrkhalil <vampirekid017@gmail.com>
2024-06-10 19:37:29 +0300

    Display likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,20 @@
-function displayLikes() {}
+function displayLikes(likees) {
+    switch (true) {
+        case likees.length == 0:
+            return "no one likes this"
+            break;
+        case likees.length == 1:
+            return `${likees[0]} likes this`
+        case likees.length == 2:
+            return `${likees[0]} and ${likees[1]} like this`
+        case likees.length == 3:
+            return `${likees[0]}, ${likees[1]} and ${likees[2]} like this`
+        case likees.length > 3:
+            return `${likees[0]}, ${likees[1]} and ${likees.length - 2} others like this`
+        default:
+            break;
+    }
+
+}
 
 module.exports = displayLikes;
