f3b3b8bbc5468f9d56e6ed6e3abfc89ca6245e8a
ibrkhalil <vampirekid017@gmail.com>
2024-06-10 20:24:32 +0300

    Find missing letter

--- 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
+++ 02-basic-challenges-2/05-find-missing-letter/find-missing-letter.js
@@ -1,3 +1,22 @@
-function findMissingLetter() {}
+function findMissingLetter(array) {
+  const lowerCased = array[0].toLowerCase() === array[0];
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  alphabet = lowerCased
+    ? "abcdefghijklmnopqrstuvwxyz"
+    : "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+
+  const startIdx = lowerCased
+    ? alphabet.indexOf(array[0].toLowerCase())
+    : alphabet.indexOf(array[0].toUpperCase());
+  const endIdx = lowerCased
+    ? alphabet.indexOf(array[array.length - 1].toLowerCase())
+    : alphabet.indexOf(array[array.length - 1].toUpperCase());
+  const searchString = alphabet.substring(startIdx, endIdx + 1);
+  for (let index = 0; index < searchString.length; index++) {
+    if (!array.includes(searchString[index])) {
+      return searchString[index];
+    }
+  }
+}
 
 module.exports = findMissingLetter;
