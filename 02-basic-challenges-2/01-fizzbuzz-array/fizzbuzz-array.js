5105d69851eeed954d0881e5d9d92495ffa088b3
ibrkhalil <vampirekid017@gmail.com>
2024-06-10 19:19:11 +0300

    FizzBuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,17 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+    const resArr = [];
+    for (let index = 1; index <= num; index++) {
+        if(index % 3 == 0 && index % 5 == 0) {
+            resArr.push('FizzBuzz')
+        } else if (index % 3 == 0) {
+            resArr.push('Fizz');
+        } else if (index % 5 == 0) {
+            resArr.push('Buzz');
+        } else
+            resArr.push(index);        
+    }
+    return resArr;
+
+}
 
 module.exports = fizzBuzzArray;
